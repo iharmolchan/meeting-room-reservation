aa26add78b035d3035459a5252b2d72cac13576e
Ihar Molchan <igor.molchan1992@gmail.com>
2022-03-08 19:44:03 +0300

    Initial commit

---
+++ src/main/java/com/iharmolchan/meetingroomreservation/MeetingRoomReservationApplication.java
@@ -0,0 +1,13 @@
+package com.iharmolchan.meetingroomreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iharmolchan/meetingroomreservation/MeetingRoomReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iharmolchan.meetingroomreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
