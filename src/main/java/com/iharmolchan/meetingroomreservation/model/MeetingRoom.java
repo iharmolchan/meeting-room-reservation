07236cd88041dabdd26f930a7cee7ce24ffe4b9e
Ihar Molchan <igor.molchan1992@gmail.com>
2022-03-14 21:02:02 +0300

    Add json view to meeting room entity

--- src/main/java/com/iharmolchan/meetingroomreservation/model/MeetingRoom.java
+++ src/main/java/com/iharmolchan/meetingroomreservation/model/MeetingRoom.java
@@ -1,6 +1,8 @@
 package com.iharmolchan.meetingroomreservation.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.iharmolchan.meetingroomreservation.views.DefaultView;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,14 +17,16 @@ import javax.persistence.Table;
 import java.util.List;
 
 
-@JsonIgnoreProperties(value = { "floor" })
+@JsonIgnoreProperties(value = {"floor", "reservations"})
 @Entity
 @Table(name = "meeting_rooms")
 @NoArgsConstructor
 @Getter
 @Setter
 public class MeetingRoom extends BaseEntity {
+    @JsonView({DefaultView.CREATE.class, DefaultView.UPDATE.class})
     private int capacity;
+    @JsonView({DefaultView.CREATE.class, DefaultView.UPDATE.class})
     private boolean multimediaCapability;
 
     @ManyToOne
