bd53e727fbc8addc84edcde1f27957dc745f03f5
Ihar Molchan <igor.molchan1992@gmail.com>
2022-03-16 12:40:44 +0300

    Remove logging annotation from main class

--- src/main/java/com/iharmolchan/meetingroomreservation/MeetingRoomReservationApplication.java
+++ src/main/java/com/iharmolchan/meetingroomreservation/MeetingRoomReservationApplication.java
@@ -1,12 +1,10 @@
 package com.iharmolchan.meetingroomreservation;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @RequiredArgsConstructor
-@Slf4j
 @SpringBootApplication
 public class MeetingRoomReservationApplication {
     public static void main(String[] args) {
